68a605fe8f5f0f14121c26a782d52f3404953d80
AndrewSpano <andrewspanopoulos@gmail.com>
2020-10-25 02:49:05 +0300

    made interface more clear

--- exc2/ClusterMain.cpp
+++ exc2/ClusterMain.cpp
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[]) {
 
   /* define the variables */
   interface::ExitCode status;
-  interface::Dataset data;
+  interface::Dataset dataset;
   interface::IOCFiles files;
   interface::input::clustering::clustering_input cluster_input;
   interface::input::clustering::clustering_config cluster_config;
@@ -27,27 +27,19 @@ int main(int argc, char const *argv[]) {
   }
 
   /* parse dataset */
-  int ret3 = interface::ParseDataset(files.input_file, data);
+  int ret3 = interface::ParseDataset(files.input_file, dataset);
 
   /* show input */
   std::cout << "complete: " << cluster_input.complete << ", method: " << cluster_input.algorithm << std::endl;
   std::cout << "\nClusters: " << cluster_config.clusters_K << ", L = " << +cluster_config.LSH_L << ", LSH_k = " << +cluster_config.LSH_k
             << ", HC_M = " << cluster_config.HC_M << ", HC_k = " << +cluster_config.HC_k << ", HC_probes = " << cluster_config.HC_probes << "\n\n";
 
+  /* create a Data object that will be used to move around the data */
+  interface::Data<uint8_t> data(dataset);
 
-  clustering::Clustering<uint8_t> cluster(cluster_config, data);
-  cluster.perform_clustering(data, "Classic");
+  clustering::Clustering<uint8_t> cluster(cluster_config, dataset);
+  cluster.perform_clustering(dataset, "Classic");
 
-  clustering::ClusterCenter<uint8_t>** centers = cluster.get_centers();
-
-  /* print images from center 0 */
-  for (int i = 0; i < 10; i++)
-  {
-    std::cout << "Image: " << i << "\n\n";
-    uint32_t index = centers[0]->vectors_in_cluster[0][i];
-    interface::output::printImage(data.images[index], data.rows_per_image, data.columns_per_image);
-    std::cout << "\n\n";
-  }
 
 
 
@@ -61,6 +53,6 @@ int main(int argc, char const *argv[]) {
 
 
   /* free up the allocated space and return */
-  interface::freeDataset(data);
+  interface::freeDataset(dataset);
   return 0;
 }
--- include/interfaces/HCUBE_interface.h
+++ include/interfaces/HCUBE_interface.h
@@ -15,17 +15,17 @@ namespace interface
   {
 
     /* namespace regarding LSH */
-    namespace HCUBE
+    namespace HC
     {
       /* struct used to define the LSH input (from the command line), the rest (epsilon, c) can be found in a configfile */
-      typedef struct HCUBE_input
+      typedef struct HCInput
       {
         uint8_t k = 4;
         uint32_t M = 10;
         uint16_t probes = 2;
         uint32_t N = 1;
         double R = 1.0;
-      } LSH_input;
+      } HCInput;
 
       /* function to parse command line parameters */
       int HCUBEParseInput(const int& argc, const char* argv[], HCUBE_input& input, IOFiles& files, ExitCode& status);
