fa73c5d1db9d48f9e988b86b506ac05a4b0a08a6
Demetris Konstantinides <demetris.konst@gmail.com>
2020-10-29 02:40:38 +0200

    Fixed bugs and added buildOutput on Hypercube

--- exc1/HCMain.cpp
+++ exc1/HCMain.cpp
@@ -24,32 +24,28 @@ int main(int argc, char const *argv[]) {
 
   /* parse dataset */
   int ret3 = interface::ParseDataset(files.input_file, dataset);
-
   interface::Data<uint8_t> data(dataset);
-
-  // std::cout << hc_input.k << '\n';
-  // LSH lshmain = LSH(data.number_of_images, 16, 28*28, lsh_input.k, lsh_input.L, lsh_input.R, pow(2,32)-5, data.images);
-  Hypercube<uint8_t> hcmain = Hypercube<uint8_t>(hc_input, data);
-
   /* parse query set */
   int ret4 = interface::ParseDataset(files.query_file, queries);
 
-  for (int i = 0; i < 5; i++) {
-    std::vector<std::pair<int, Item<uint8_t>*>> kNNRes = hcmain.ApproxNN(queries.images[i], hc_input.N, hc_input.probes, hc_input.M);
+  interface::output::KNNOutput output;
+  output.n = 5;
+  output.R = 0.0;
+  output.method = "Hypercube";
 
-    std::cout << "----" << i << "----" << '\n';
-    for (int j = 0; j < kNNRes.size(); j++) {
-      std::cout << j << "th Distance - ApproxNN " << kNNRes[j].first << ", Item -> " << kNNRes[j].second->id << '\n';
-    }
+  BruteForce<uint8_t> bf = BruteForce<uint8_t>(data);
+  // clock_t begin = clock();
+  // double averageItemDistance = bf.averageDistance(0.05);
+  // clock_t end = clock();
+  // double elapsed = double(end - begin) / CLOCKS_PER_SEC;
+  // std::cout << "avgItemDist: " << averageItemDistance << '\n';
+  // std::cout << "Time for avgItemDist: " << elapsed << '\n';
+  bf.buildOutput(output, queries, hc_input.N);
 
-    std::vector<std::pair<int, Item<uint8_t>*>> rsRes = hcmain.RangeSearch(queries.images[i], hc_input.R, hc_input.probes, hc_input.M);
+  Hypercube<uint8_t> hc = Hypercube<uint8_t>(hc_input, data, 10000);
+  hc.buildOutput(output, queries, hc_input.N, hc_input.R, hc_input.probes, hc_input.M);
 
-    std::cout << "----" << i << "----" << '\n';
-    for (int j = 0; j < rsRes.size(); j++) {
-      std::cout << j << "th Distance - RangeSearch" << rsRes[j].first << ", Item -> " << rsRes[j].second->id << '\n';
-    }
-
-  }
+  interface::output::writeOutput(files.output_file, output, status);
 
   return 0;
 }
--- include/Hypercube/Hypercube.hpp
+++ include/Hypercube/Hypercube.hpp
@@ -1,4 +1,4 @@
-#include <unordered_map>
+#include <random>
 
 #include "../core/item.hpp"
 #include "../LSH/LSHFun.hpp"
@@ -6,75 +6,69 @@
 #include "../interfaces/HC_interface.h"
 #include "../utils/lsh_hc.hpp"
 
+class FFunc {
+private:
+  unsigned int randomNumber;
+
+public:
+  FFunc () {
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+    std::uniform_int_distribution<int> distribution(100, 200);
+
+    randomNumber = distribution(generator);
+  }
+
+  int map (unsigned int hVal) {
+    return hVal >= randomNumber;
+  }
+};
+
 template <typename T>
 class Hypercube {
 private:
-  int n;
-  int HCdim;
-  int datadim;
-  double searchRadius;
-  int m;
+  int imageCount;
+  int HCDimension;
+  int dataDimension;
+  double averageItemDistance;
 
+  std::vector<FFunc> mapFun;
   std::vector<HashFunction<T>> LSHFun;
 
   std::vector<Item<T>*>* H;
-public:
-  Hypercube(interface::input::HC::HCInput& hci, const interface::Data<T>& ds) {
-    HCdim = hci.k;
-    searchRadius = hci.R;
-    n = ds.n;
-    datadim = ds.dimension;
-    Item<T>** items = ds.items;
-    m = pow(2, 32) - 5;
 
-    int vertexCount = pow(2, HCdim);
+  // The following function maps the result of H to {0, 1}
 
-    H = new std::vector<Item<T>*>[vertexCount];
-
-    for (int i = 0; i < vertexCount; i++) {
-      std::vector<Item<T>*> tmpVec;
-      H[i] = tmpVec;
-    }
-
-    long int mConstant = pow(2, 32) - 5;
-    int* mmod = new int[datadim];
-    for (int b = 0; b < datadim; b++)
-      mmod[b] = utils::modEx(mConstant, datadim-b-1, pow(2, 32/HCdim));
-
-    for (int i = 0; i < HCdim; i++) {
-      LSHFun.push_back(HashFunction<T>(searchRadius, 4, 4, datadim, mmod));
-    }
-
-    for (int i = 0; i < n; i++) {
-      int vertex = calculateVertex(items[i]->data);
-      H[vertex].push_back(items[i]);
-
-      if ((i+1)%10000 == 0)
-        std::cout << i+1 << " items..." << '\n';
-    }
-  }
-
-  int calculateVertex (T* x) {
+  int _calculateVertex (T* x) {
     int vertex = 0;
-    for (int i = 0; i < HCdim; i++) {
-      T hashRes = LSHFun[i].HashVector(x);
 
-      if (hashRes%2 == 1) {
+    for (int i = 0; i < HCDimension; i++) {
+      int hashRes = LSHFun[i].HashVector(x);
+      // std::cout << hashRes << "->";
+      int mapRes = mapFun[i].map(hashRes);
+
+      std::cout << mapRes;
+
+      if (mapRes == 1) {
         vertex |= 1;
       }
 
-      if (i < HCdim-1)
+      if (i < HCDimension-1)
         vertex <<= 1;
-
     }
+
+    std::cout << '\n';
 
     return vertex;
   }
 
-  std::vector<int> getProbesPerHD (int dist, int startingVertex) {
+  /*
+  The following function returns all vertex indices of distance "dist" from startingVertex
+  */
+  std::vector<int> _getProbesPerHD (int dist, int startingVertex) {
     std::vector<int> vec;
 
-    for (int i = 0; i < pow(2, HCdim); i++) {
+    for (int i = 0; i < pow(2, HCDimension); i++) {
       if (metrics::HammingdDistance(startingVertex, i) == dist){
         vec.push_back(i);
       }
@@ -83,11 +77,14 @@ public:
     return vec;
   }
 
-  std::vector<std::vector<Item<T>*>> getAvailableProbes (int probes, int startingVertex) {
+  /*
+  This function returns an array of "probes" amount of the closest vertices to startingVertex
+  */
+  std::vector<std::vector<Item<T>*>> _getAvailableProbes (int probes, int startingVertex) {
     std::vector<std::vector<Item<T>*>> probeVec;
 
-    for (int i = 0; i < HCdim; i++) {
-      std::vector<int> pph = getProbesPerHD(i, startingVertex);
+    for (int i = 0; i < HCDimension; i++) {
+      std::vector<int> pph = _getProbesPerHD(i, startingVertex);
 
       int a = 0;
       for (int j = probeVec.size(); j < probes && a < pph.size(); j++, a++) {
@@ -102,19 +99,53 @@ public:
     return probeVec;
   }
 
-  std::vector<std::pair<int, Item<T>*>> kNN (T* query, int N, int probes, int thresh) {
+public:
+  Hypercube(interface::input::HC::HCInput& hci, const interface::Data<T>& ds, double avg) : averageItemDistance(avg) {
+    HCDimension = hci.k;
+    imageCount = ds.n;
+    dataDimension = ds.dimension;
+    Item<T>** items = ds.items;
+
+    int vertexCount = pow(2, HCDimension);
+
+    H = new std::vector<Item<T>*>[vertexCount];
+    for (int i = 0; i < vertexCount; i++) {
+      std::vector<Item<T>*> tmpVec;
+      H[i] = tmpVec;
+    }
+
+    long int mConstant = pow(2, 32) - 5;
+    int* mmod = new int[dataDimension];
+    for (int b = 0; b < dataDimension; b++)
+      mmod[b] = utils::modEx(mConstant, dataDimension-b-1, pow(2, 32/HCDimension));
+
+    for (int i = 0; i < HCDimension; i++) {
+      LSHFun.push_back(HashFunction<T>(averageItemDistance, 4, 4, dataDimension, mmod));
+      mapFun.push_back(FFunc());
+    }
+
+    for (int i = 0; i < imageCount; i++) {
+      int vertex = _calculateVertex(items[i]->data);
+      H[vertex].push_back(items[i]);
+
+      if ((i+1)%10000 == 0)
+        std::cout << i+1 << " items..." << '\n';
+    }
+  }
+
+  std::vector<std::pair<int, Item<T>*>> kNN (T* query, int N, int probes, int thresh = 0) {
     std::vector<std::pair<int, Item<T>*>> d;
 
     for (int i = 0; i < N; i++)
       d.push_back(std::make_pair(std::numeric_limits<int>::max(), new Item<T>()));
 
-    int vertex = calculateVertex(query);
-    std::vector<std::vector<Item<T>*>> avProbes = getAvailableProbes(probes, vertex);
+    int vertex = _calculateVertex(query);
+    std::vector<std::vector<Item<T>*>> avProbes = _getAvailableProbes(probes, vertex);
 
     int itemsSearched = 0;
     for (int i = 0; i < avProbes.size(); i++) {
       for (int j = 0; j < avProbes[i].size(); j++) {
-        int distance = metrics::ManhattanDistance<T>(query, avProbes[i][j]->data, datadim);
+        int distance = metrics::ManhattanDistance<T>(query, avProbes[i][j]->data, dataDimension);
 
         if (distance < d[N-1].first) {
           d[N-1].first = distance;
@@ -122,7 +153,7 @@ public:
           std::sort(d.begin(), d.end(), comparePairs<T>);
         }
 
-        if (++itemsSearched >= thresh)
+        if (thresh != 0 && ++itemsSearched >= thresh)
           return d;
       }
     }
@@ -133,8 +164,8 @@ public:
   std::vector<std::pair<int, Item<T>*>> RangeSearch (T* query, double radius, int probes, int thresh) {
     std::vector<std::pair<int, Item<T>*>> d;
 
-    int vertex = calculateVertex(query);
-    std::vector<std::vector<Item<T>*>> avProbes = getAvailableProbes(probes, vertex);
+    int vertex = _calculateVertex(query);
+    std::vector<std::vector<Item<T>*>> avProbes = _getAvailableProbes(probes, vertex);
 
     int itemsSearched = 0;
     for (int i = 0; i < avProbes.size(); i++) {
@@ -142,7 +173,7 @@ public:
         if (avProbes[i][j]->marked)
           continue;
 
-        int distance = metrics::ManhattanDistance<T>(query, avProbes[i][j]->data, datadim);
+        int distance = metrics::ManhattanDistance<T>(query, avProbes[i][j]->data, dataDimension);
 
         if (distance < radius) {
           std::pair<int, Item<T>*> tmpPair = std::make_pair(distance, avProbes[i][j]);
@@ -157,13 +188,49 @@ public:
     return d;
   }
 
-  std::vector<std::vector<std::pair<int, Item<T>*>>> buildOutput (T** query, int N, int probes, int thresh = 0) {
-    std::vector<std::vector<std::pair<int, Item<T>*>>> hcVec;
+  void buildOutput (interface::output::KNNOutput& output, interface::Dataset& query, int N, double R, int probes, int thresh = 0) {
+    std::vector<std::vector<int>> neighborIdVec;
+    std::vector<std::vector<double>> distVec;
+    std::vector<double> timeVec;
+    std::vector<std::vector<int>> rsIdVec;
 
-    for (int i = 0; i < query.number_of_images; i++) {
-      hcVec.push_back(kNN(query[i], N, probes, thresh));
+    for (int i = 0; i < 5; i++) {
+      std::vector<int> tmpNVec;
+      std::vector<double> tmpDistVec;
+
+      clock_t begin = clock();
+
+      std::vector<std::pair<int, Item<T>*>> kNNRes = kNN(query.images[i], N, probes, thresh);
+
+      clock_t end = clock();
+      double elapsed = double(end - begin) / CLOCKS_PER_SEC;
+
+      for (int j = 0; j < kNNRes.size(); j++) {
+        if (!kNNRes[j].second->null) {
+          tmpNVec.push_back(kNNRes[j].second->id);
+          tmpDistVec.push_back((double) kNNRes[j].first);
+        }
+      }
+
+      std::vector<int> tmpRsVec;
+
+      std::vector<std::pair<int, Item<T>*>> rsRes = RangeSearch(query.images[i], R, probes, thresh);
+
+      for (int j = 0; j < rsRes.size(); j++) {
+        if (!rsRes[j].second->null) {
+          tmpRsVec.push_back(rsRes[j].second->id);
+        }
+      }
+
+      neighborIdVec.push_back(tmpNVec);
+      distVec.push_back(tmpDistVec);
+      timeVec.push_back(elapsed);
+      rsIdVec.push_back(tmpRsVec);
     }
 
-    return hcVec;
+    output.n_neighbors_id = neighborIdVec;
+    output.approx_distance = distVec;
+    output.approx_time = timeVec;
+    output.r_near_neighbors_id = rsIdVec;
   }
 };
